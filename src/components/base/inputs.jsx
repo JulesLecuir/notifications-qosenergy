43f11f7b1003377b228ab72760e0699c036662e9
Jules Lecuir <jules.lecuir@outlook.fr>
2023-01-08 14:30:05 +0100

    Add dark theme compatibility to Antd components

--- src/components/base/inputs.jsx
+++ src/components/base/inputs.jsx
@@ -8,9 +8,10 @@ import Sheet from "@mui/joy/Sheet";
 import TreeView from "@mui/lab/TreeView";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore.js";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight.js";
-import {Tree} from "antd";
+import {ConfigProvider, theme, Tree} from "antd";
 import Stack from "@mui/joy/Stack";
 import {Checkbox, Chip} from "@mui/joy";
+import {useColorScheme} from "@mui/joy/styles";
 
 export const DiscoverableTextField = ({discoverFieldLabel, ...props}) => {
   const [discovered, setDiscovered] = useState(props.value?.length > 0);
@@ -55,20 +56,35 @@ export const MuiTree = ({options, label, name, value, setFieldValue}) => {
   );
 };
 
+// Change AntDesign theme in the same time as MUI theme
+const AntdToMuiThemeLinking = ({children}) => {
+  const {defaultAlgorithm, darkAlgorithm} = theme;
+  const {mode} = useColorScheme();
+  const isDarkMode = mode === "dark";
+
+  return (
+    <ConfigProvider theme={{algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm}}>
+      {children}
+    </ConfigProvider>
+  );
+};
+
 export const AntdTree = ({options, label, name, value, setFieldValue}) => {
   return (
-    <FormControl>
-      <FormLabel>{label}</FormLabel>
-      <Sheet variant="outlined" sx={{borderRadius: 2, p: 2, maxHeight: 400, overflow: "auto"}}>
-        <Tree
-          checkable
-          defaultExpandAll
-          checkedKeys={value}
-          onCheck={(checkedKeys) => setFieldValue(name, checkedKeys)}
-          treeData={options}
-        />
-      </Sheet>
-    </FormControl>
+    <AntdToMuiThemeLinking>
+      <FormControl>
+        <FormLabel>{label}</FormLabel>
+        <Sheet variant="outlined" sx={{borderRadius: 2, p: 2, maxHeight: 400, overflow: "auto"}}>
+          <Tree
+            checkable
+            defaultExpandAll
+            checkedKeys={value}
+            onCheck={(checkedKeys) => setFieldValue(name, checkedKeys)}
+            treeData={options}
+          />
+        </Sheet>
+      </FormControl>
+    </AntdToMuiThemeLinking>
   );
 };
 
